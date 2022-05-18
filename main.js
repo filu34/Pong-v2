72a914d17c3ec4b3e2ae0794bd125a587106fac4
PostR <filu3@protonmail.com>
2022-05-18 19:56:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+import calculateMousePos from
+
+const main = function() {
+
+	const canvas;
+	const canvasContext;
+
+	const rectWidth = 10;
+	const rectHeight = 10;
+
+	let ballX = 400 - 5;
+	let ballSpeedX = 5;
+	
+	let ballY = 300 - 5;
+	let ballSpeedY = 2;
+
+	let player1Score = 0;
+	let player2Score = 0;
+
+	const WINNING_SCORE = 3;
+
+	let showWinScreen = false;
+
+	let paddle1Y = 250;
+	let paddle2Y = 250;
+	const PADDLE_HEIGHT = 100;
+	const PADDLE_THICKNESS = 10;
+
+
+};
